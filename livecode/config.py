f483bcc2f71313885a0246e6379729a2cb1f8751
Aris Zaatri <az.poly2024@gmail.com>
2026-02-12 19:47:57 -0500

    Added a comment to not forget differents LLM_MODELs

--- livecode/config.py
+++ livecode/config.py
@@ -6,4 +6,4 @@ load_dotenv()
 DB_PATH = "./inf1900_db"
 ENTRY_URL = "https://cours.polymtl.ca/inf1900/"
 EMBEDDING_MODEL = "gemini-embedding-001"
-LLM_MODEL = "gemini-flash-latest"
+LLM_MODEL = "gemini-flash-latest" # peut être changé pour "gemini-pro-latest" si vous avez accès à ce modèle plus puissant
